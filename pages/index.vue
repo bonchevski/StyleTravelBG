d961821404f0d6495f8008ff81d57d2f49f97cf3
Robert <robert13mitroi@gmail.com>
2018-05-27 23:55:15 +0200

    no message

--- pages/index.vue
+++ pages/index.vue
@@ -87,6 +87,33 @@
             </div>
 
         </section>
+
+
+
+        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" @click="!displayModal">
+        Launch demo modal
+        </button>
+
+        <!-- Modal -->
+        <div class="modal" id="exampleModal" role="dialog" aria-labelledby="exampleModalLabel" v-show="displayModal">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        ...
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </main>
 
 </template>
@@ -102,6 +129,12 @@
         components: {
             Banner
         },
+        data() {
+            return {
+                displayModal: false
+            }
+        }
+
 
     }
 
